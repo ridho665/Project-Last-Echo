c28e6a2090d40a92416733d3574b51f173eae976
Maghribii <116049090+ridho665@users.noreply.github.com>
2024-09-11 00:32:22 +0700

    Add Cutscene in TimeLine, Add UI_Paused

--- Assets/Scripts/InGameUI.cs
+++ Assets/Scripts/InGameUI.cs
@@ -1,32 +1,67 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class InGameUI : MonoBehaviour
 {
-    [SerializeField] private RectTransform batteryBar; // Referensi ke RectTransform dari UI Battery Bar
+    [SerializeField] private GameObject inGameUI;
+    [SerializeField] private GameObject pauseUI;
+    [SerializeField] private RectTransform batteryBar;
 
     private PlayerBattery playerBattery;
+    private bool gamePaused;
 
     private void Start()
     {
         PlayerManager.instance.inGameUI = this;
-        // // Ambil referensi ke PlayerBattery dari PlayerManager
-        // playerBattery = PlayerManager.instance.GetPlayerBattery();
-
-        // // Set RectTransform batteryBar di PlayerBattery
-        // if (playerBattery != null && batteryBar != null)
-        // {
-        //     playerBattery.SetBatteryBar(batteryBar); // Hubungkan batteryBar UI dengan PlayerBattery
-        // }
+        Time.timeScale = 1;
+        SwitchUI(inGameUI); 
     }
 
     private void Update()
     {
         UpdateInGameInfo();
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            CheckIfNotPaused();
+    }
+
+    private bool CheckIfNotPaused()
+    {
+        if (!gamePaused)
+        {
+            gamePaused = true;
+            Time.timeScale = 0;
+            SwitchUI(pauseUI);
+            return true;
+        }
+        else
+        {
+            gamePaused = false;
+            Time.timeScale = 1;
+            SwitchUI(inGameUI);
+            return false;
+        }
     }
 
     private void UpdateInGameInfo()
     {
         playerBattery = PlayerManager.instance.GetPlayerBattery();
-    }   
+    }
+
+    public void SwitchUI(GameObject uiMenu)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
+
+        uiMenu.SetActive(true);
+    }  
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    } 
 }
