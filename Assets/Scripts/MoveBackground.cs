3df8ec8f74f4162adcf5ddc3afb2e5d38042cf0b
Yuukata <gsumtopia@gmail.com>
2024-09-12 14:34:29 +0700

    Input Background + Parallax
    Parallax Script, building background,Sky Background,Cloud Background,Moon Background

---
+++ Assets/Scripts/MoveBackground.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBackground : MonoBehaviour
+{
+    private float lenght;
+    private float startPos;
+    private GameObject Camera;
+    [SerializeField] private float parallaxEffect;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera = GameObject.Find("Virtual Camera");
+        startPos = transform.position.x;
+        lenght = gameObject.GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float temp = (Camera.transform.position.x * (1 - parallaxEffect));
+        float distance = (Camera.transform.position.x * parallaxEffect);
+        Vector3 target = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+
+        transform.position = Vector3.Lerp(transform.position, target, Time.deltaTime * 5);
+
+        if (temp > startPos + lenght)
+        {
+            startPos += lenght;
+        }
+        else if (temp < startPos - lenght)
+        {
+            startPos -= lenght;
+        }
+
+    }
+}
